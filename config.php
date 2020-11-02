1ca5bd2771e55790f9c531e036a5ecf64dd7ba2e
ninja <dipanshu231099@gmail.com>
2020-11-02 21:27:06 +0530

    added schema for database

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'host' => 'localhost',
+    'username' => 'username',
+    'password' => 'password',
+    'dbname' => 'HP_STSE',
+);
+
+?>
--- webpages/login.php
+++ webpages/login.php
@@ -18,7 +18,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12 login-form">
-				<form action="form.php" method="post">
+				<form action="loginSubmit.php" method="post">
 					<div class="form-row">
 						<div class="form-group col-md-6">
 							<label for="inputEmail4">Email</label>
@@ -28,14 +28,6 @@
 							<label for="inputPassword4">Password</label>
 							<input type="password" class="form-control" id="inputPassword4" placeholder="Password">
 						</div>
-					</div>
-					<div class="form-group col-md-12">
-						<label for="inputAddress">Address</label>
-						<input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-					</div>
-					<div class="form-group col-md-12">
-						<label for="inputAddress2">Address 2</label>
-						<input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
 					</div>
 					<div class="form-row">
 						<div class="form-group col-md-6">
