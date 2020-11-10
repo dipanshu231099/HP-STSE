912ccc06df7ef421966ee4b1bd171e752d29b9dc
hrs786 <52928171+hrs786@users.noreply.github.com>
2020-11-10 22:01:10 +0530

    Modify Schema

--- DBSchema.sql
+++ DBSchema.sql
@@ -19,7 +19,6 @@
 -- Table structure for table `Admin`
 --
 
-USE HP_STSE;
 DROP TABLE IF EXISTS `Admin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -27,18 +26,8 @@ CREATE TABLE `Admin` (
   `username` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
   PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Admin`
---
-
-LOCK TABLES `Admin` WRITE;
-/*!40000 ALTER TABLE `Admin` DISABLE KEYS */;
-INSERT INTO `Admin` VALUES ('admin','admin');
-/*!40000 ALTER TABLE `Admin` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `Schools`
@@ -52,18 +41,12 @@ CREATE TABLE `Schools` (
   `email` varchar(100) NOT NULL,
   `password` varchar(30) NOT NULL,
   `city` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Schools`
---
-
-LOCK TABLES `Schools` WRITE;
-/*!40000 ALTER TABLE `Schools` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Schools` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `Students_2020`
@@ -76,67 +59,60 @@ CREATE TABLE `Students_2020` (
   `firstname` varchar(30) NOT NULL,
   `lastname` varchar(30) DEFAULT NULL,
   `email` varchar(100) NOT NULL,
-  `school` varchar(100) NOT NULL,
-  `category` varchar(30) DEFAULT NULL,
-  `fathername` varchar(30) DEFAULT NULL,
-  `mothername` varchar(30) DEFAULT NULL,
-  `date_of_birth` date DEFAULT NULL,
-  `contact_number` varchar(11) DEFAULT NULL,
+  `school` int unsigned DEFAULT NULL,
   `schoolRegNo` varchar(100) NOT NULL,
   `password` varchar(50) NOT NULL,
-  PRIMARY KEY (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  PRIMARY KEY (`email`),
+  KEY `school` (`school`),
+  CONSTRAINT `Students_2020_ibfk_1` FOREIGN KEY (`school`) REFERENCES `Schools` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Students_2020`
+-- Table structure for table `Students_Application_2020`
 --
-
-LOCK TABLES `Students_2020` WRITE;
-/*!40000 ALTER TABLE `Students_2020` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Students_2020` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 DROP TABLE IF EXISTS `Students_Application_2020`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Students_Application_2020` (
-  	`applicantname` varchar(30) NOT NULL,
-  	`fathername` varchar(30) DEFAULT NULL,
-	`mothername` varchar(30) DEFAULT NULL,
-   	`gender` varchar(30) DEFAULT NULL,
-   	`date_of_birth` date DEFAULT NULL,
-	`area` varchar(30) DEFAULT NULL,
-  	`caste` varchar(30) DEFAULT NULL,
-  	`disability` varchar(30) DEFAULT NULL,
-    `add1` varchar(30) DEFAULT NULL,
-  	`add2` varchar(30) DEFAULT NULL,
-  	`state` varchar(30) NOT NULL,
-  	`district` varchar(30) NOT NULL,
-  	`pincode` int NOT NULL,
-  	`type_of_ins` varchar(30) DEFAULT NULL,
-  	`stu_of_ken` varchar(30) DEFAULT NULL,
-  	`med_of_exam1` varchar(30) DEFAULT NULL,
-  	`med_of_exam2` varchar(30) DEFAULT NULL,
-  	`fedu` varchar(30) DEFAULT NULL,
-  	`focc` varchar(30) DEFAULT NULL,
-  	`medu` varchar(30) DEFAULT NULL,
-  	`mocc` varchar(30) DEFAULT NULL,
-  	`family_members` int NOT NULL,
-  	`brothers` int NOT NULL,
-  	`sisters` int NOT NULL,
-  	`level_in_family` varchar(30) DEFAULT NULL,
-  	`income` varchar(30) DEFAULT NULL,
-  	`email` varchar(100) NOT NULL,
-  	
-  	`contact_number` varchar(11) DEFAULT NULL,
-  	`aadhar` varchar(100) NOT NULL,
-  	`telephone` varchar(11) DEFAULT NULL,
-  	`exam_center` varchar(50) DEFAULT NULL,
-  	
+  `applicantname` varchar(30) NOT NULL,
+  `fathername` varchar(30) DEFAULT NULL,
+  `mothername` varchar(30) DEFAULT NULL,
+  `gender` varchar(30) DEFAULT NULL,
+  `date_of_birth` date DEFAULT NULL,
+  `area` varchar(30) DEFAULT NULL,
+  `caste` varchar(30) DEFAULT NULL,
+  `disability` varchar(30) DEFAULT NULL,
+  `add1` varchar(30) DEFAULT NULL,
+  `add2` varchar(30) DEFAULT NULL,
+  `state` varchar(30) NOT NULL,
+  `district` varchar(30) NOT NULL,
+  `pincode` int NOT NULL,
+  `type_of_ins` varchar(30) DEFAULT NULL,
+  `stu_of_ken` varchar(30) DEFAULT NULL,
+  `med_of_exam1` varchar(30) DEFAULT NULL,
+  `med_of_exam2` varchar(30) DEFAULT NULL,
+  `fedu` varchar(30) DEFAULT NULL,
+  `focc` varchar(30) DEFAULT NULL,
+  `medu` varchar(30) DEFAULT NULL,
+  `mocc` varchar(30) DEFAULT NULL,
+  `family_members` int NOT NULL,
+  `brothers` int NOT NULL,
+  `sisters` int NOT NULL,
+  `level_in_family` varchar(30) DEFAULT NULL,
+  `income` varchar(30) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `contact_number` varchar(11) DEFAULT NULL,
+  `aadhar` varchar(100) NOT NULL,
+  `telephone` varchar(11) DEFAULT NULL,
+  `exam_center` varchar(50) DEFAULT NULL,
+  `status` int DEFAULT '1',
   PRIMARY KEY (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
@@ -145,4 +121,4 @@ CREATE TABLE `Students_Application_2020` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-05 18:34:37
+-- Dump completed on 2020-11-10 21:59:39
