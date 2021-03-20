ab237e553b9675dd772e9f7794d09c3351fcba84
Ayushman Dixit <46623283+ayushman2001@users.noreply.github.com>
2021-03-20 18:42:17 +0530

    Update email-handler.php

--- webpages/email-handler.php
+++ webpages/email-handler.php
@@ -15,7 +15,7 @@
 			$mail->Host	 = 'smtp.gmail.com;';					 
 			$mail->SMTPAuth = true;							 
 			$mail->Username = 'mandibtech@gmail.com';				 
-			$mail->Password = 'mandi_hp_in';						 
+			$mail->Password = '';						 
 			$mail->SMTPSecure = 'tls';							 
 			$mail->Port	 = 587; 
 
